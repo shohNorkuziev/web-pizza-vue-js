fb8351898ff8ee7878f8e86b83d9bf3d3ff1f9f4
Shohruh Norkuziev <shohruh123777@gmail.com>
2023-04-10 04:59:25 +0500

    commit 6

--- pizza-app/src/components/Pizza.vue
+++ pizza-app/src/components/Pizza.vue
@@ -1,26 +1,37 @@
 <script>
 export default {
-  name:"Pizza",
-props: {
-  title: String,
-  price: Number
-}
+  name: "Pizza",
+  props: {
+    title: String,
+    price: Number
+  }
 }
 
 </script>
 
 <template>
-  <div >
+  <div>
     <img class="pizza-block__image"
       src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
       alt="Pizza" />
     <h4 class="pizza-block__title">{{ title }}</h4>
     <div class="pizza-block__selector">
       <ul>
-        <li class="active">тонкое</li>
-        <li>традиционное</li>
+        <li class="active">
+          <label for="dough-thin">
+            <input type="radio" name="dough" id="dough-thin">
+            тонкое
+          </label>
+        </li>
+        <li>
+          <label for="dough-traditional">
+            <input type="radio" name="dough" id="dough-traditional">
+            традиционное
+          </label>
+        </li>
       </ul>
       <ul>
+        
         <li class="active">26 см.</li>
         <li>30 см.</li>
         <li>40 см.</li>
