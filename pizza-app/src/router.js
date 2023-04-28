08a7f4651b9ced63ced054134d237a670d8c6488
Shohruh Norkuziev <shohruh123777@gmail.com>
2023-04-28 21:12:28 +0500

    edit styles pizzas

--- pizza-app/src/router.js
+++ pizza-app/src/router.js
@@ -1,7 +1,7 @@
 import { createRouter,createWebHashHistory } from 'vue-router'
 
-import Basket from './components/Basket.vue'
-import Index from './components/Index.vue'
+import Basket from './components/basket.vue'
+import Index from './components/index.vue'
 import SignInVue from './components/SignIn.vue'
 import SignUpVue from './components/SignUp.vue'
 
